141b05ee29ba4836c623409d39f4790cf6e2af7d
briangor <briangor13@gmail.com>
2024-05-08 17:21:44 +0300

    update MinPerimeterRectangle.java

--- 10-prime-composite-nums/MinPerimeterRectangle.java
+++ 10-prime-composite-nums/MinPerimeterRectangle.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class MinPerimeterRectangle {
     public static void main(String[] args) {
         int N = 30;
@@ -5,6 +7,35 @@ public class MinPerimeterRectangle {
     }
 
     public static int solution(int N) {
+        int min = 0;
+
+        // find factors
+        ArrayList<Integer> factors = new ArrayList<>();
+        for(int i = 1; i <= N; i++) {
+            if(N % i == 0) {
+                System.out.print(i + " ");
+                factors.add(i);
+            } 
+        }
+        System.out.println("\n");
+
+        //check for vals
+        for(int i = 0; i < factors.size(); i++) {
+            //System.out.print(factors.get(i) + " ");
+            if(i != factors.size()) { 
+                for(int j = 0; j < factors.size(); j++) {
+                // System.out.print(factors.get(j) + " ");
+        
+                int prod = factors.get(i) * factors.get(i+1);
+                if(prod == N && prod > min && j != factors.size()) {
+                    min = prod;
+                }
+            }
+            }
+            
+            System.out.println(" ");
+        }
+        System.out.println("\n");
         return N;
     }
 }
