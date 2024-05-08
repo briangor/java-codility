e47495b2a808fd70934a6a7b02226a16c5046e26
briangor <briangor13@gmail.com>
2024-05-08 17:00:37 +0300

    add MinPerimeterRectangle.java

---
+++ 10-prime-composite-nums/MinPerimeterRectangle.java
@@ -0,0 +1,10 @@
+public class MinPerimeterRectangle {
+    public static void main(String[] args) {
+        int N = 30;
+        System.out.println(solution(N));
+    }
+
+    public static int solution(int N) {
+        return N;
+    }
+}
