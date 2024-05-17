edc9b9c75c93be2ecbd16cc0b5eef39401d82a8d
briangor <briangor13@gmail.com>
2024-05-17 17:31:36 +0300

    add CountDistinctSlices.java

---
+++ 15-caterpillar-method/CountDistinctSlices.java
@@ -0,0 +1,42 @@
+public class CountDistinctSlices {
+    public static void main(String[] args) {
+        int M = 6;
+        int[] A = { 3, 4, 5, 5, 2 };
+        System.out.println(solution(M, A));
+        System.out.println("--------");
+
+    }
+
+    public static int solution(int M, int[] A) {
+        int count = 0;
+
+        // print all 
+        /* for(int i = 0; i < A.length; i++) {
+            System.out.print(i+": ");
+            for(int j = i; j < A.length; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.print("\n\n");
+        } */
+
+
+        // op
+        for(int i = 0; i < A.length; i++) {
+            System.out.print(i+": ");
+            for(int j = i; j < A.length; j++) {
+                // condition to check for occurrence
+                // add(i,i), add(i, i+1) if unique 
+                // if(A[i] < A[j]) okay 
+                // System.out.print("("+i+", "+j+")");
+                
+                if(A[i] <= A[j]) {
+                    System.out.print("("+i+", "+j+")");
+                }
+
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+        return 0;
+    }
+}
