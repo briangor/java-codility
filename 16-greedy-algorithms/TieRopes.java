e4c49d93f59c6456065e05d156dd3b908a097502
briangor <briangor13@gmail.com>
2024-07-12 16:46:45 +0300

    add TieRopes.java

---
+++ 16-greedy-algorithms/TieRopes.java
@@ -0,0 +1,11 @@
+public class TieRopes {
+    public static void main(String[] args) {
+        int K = 10;
+        int[] A = {1,2,3};
+        System.out.print(solution(K, A));
+    }
+
+    public static int solution(int K, int[] A) {
+        return K;
+    }
+}
